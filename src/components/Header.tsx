a1880632ba6ff463b3b6716768a69ccdf952be2e
cestari15 <rafacjacobs1502@gmail.com>
2024-08-01 15:14:11 -0300

    Commit 01/08

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import styles from '../components/Header.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import logo from "../images/loguinho.png";
+import logo from "../images/logo.png";
 
 const Navbar = () => {
   return (
 <header>
- <img  src={logo}  className={styles.logo} />
+
 <nav>
-  <ul className={styles.navlinks}>
-    <li><a href="#">Dieta</a></li>
-    <li><a href="">Nutri</a></li>
-    <li><a href="#">Refeição</a></li>
-  </ul>
+ 
+ 
+ 
 </nav>
-<a className={styles.cta} href=""><button>Login</button></a>
+
 </header>
   );
 }
--- src/components/PaginaHome.tsx
+++ src/components/PaginaHome.tsx
@@ -1,7 +1,8 @@
 import React, { Component, useState, ChangeEvent, FormEvent, useEffect } from "react";
 import styles from '../components/Home.module.css'
 import Navbar from "./Header";
-import logo from "../images/logo2.png"
+import logo from "../images/logo.png"
+import banner from "../images/banner.jpg"
 
 const PaginaHome = () => {
     return (
@@ -12,12 +13,34 @@ const PaginaHome = () => {
             <div className={styles.banner}>
 
                 <img src={logo} className={styles.logo} />
-                <h5>Melhore sua vida com novos hábitos alimentares</h5>
+                <h5 className={styles.slogan}>Descubra o Sabor da Saúde</h5>
+                <h5 className={styles.slogan2}>Nutrição Personalizada ao seu Alcance!</h5>
             </div>
 
-            <div >
+            <div className={styles.banner2}>
+                <h5 className={styles.text1}>Tratamento com os melhores nutricionistas</h5>
+                <h4 className={styles.text2}>Acompanhe sua dieta aqui!</h4>
+
+                <img src={banner} className={styles.imagem} />
+            </div>
+
+            <div className={styles.banner3}>
+                <h2 className={styles.titulo}>Sabor & Saúde</h2>
+
+
+                <h5 className={styles.subtitle}>Você sabia ?</h5>
+                <h5 className={styles.subtitle1}>Basicamente o gasto energético do metabolismo humano é dividido em:</h5>
+
+                <div className="">
+
+                </div>
+
+                <p className={styles.p1}>Quando se faz uma dieta, </p>
+                <p className={styles.p2}>todos os gastos energéticos diminuem, mas a massa muscular é a mais afetada, pois ela sofre </p>
+                <p className={styles.p3}>degradação durante o emagrecimento.</p>
 
             </div>
+
         </div>
 
 
